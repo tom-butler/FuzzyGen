82fdaaec4325afa7db40da07dac73827aa4f6864
Tom Butler <tombutler.development@gmail.com>
2014-08-22 16:01:32 +1000

    removing old template files

--- src/sims/_template/template.h
+++
@@ -1,8 +0,0 @@
-#ifndef FUZZYGEN_SIMS_TEMPLATE_H
-#define FUZZYGEN_SIMS_TEMPLATE_H
-
-void TemplateCreateVars();
-void TemplateInitSim(int controller);
-int TemplateNextStep(int controller);
-
-#endif
--- src/sims/_template/templatedraw.h
+++
@@ -1,8 +0,0 @@
-#ifndef FUZZYGEN_SIMS_TEMPLATE_DRAW_H
-#define FUZZYGEN_SIMS_TEMPLATE_DRAW_H
-
-void InitTemplateDraw();
-void DrawTemplateSets(int window);
-void DrawTemplateSim(int window);
-
-#endif
