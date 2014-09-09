f455feaef9b109f36bb03003a9c09a303df28d84
Tom Butler <tombutler.development@gmail.com>
2014-09-09 14:28:27 +1000

    adding the catch testing framework

---
+++ src/cmd/catch.cpp
@@ -0,0 +1,10 @@
+/**
+ * Unit Tests for FuzzyGen
+ *
+ * This file uses catch (a c++ unit testing framework) 
+ * to test the system for un-expected behaviou
+ */
+
+#define CATCH_CONFIG_MAIN
+#include "..\..\libs\catch.hpp"
+
