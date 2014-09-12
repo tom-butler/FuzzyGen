936b535ecb8c3e5375a23992f7e4ea32a7c3812d
Tom Butler <tombutler.development@gmail.com>
2014-09-12 14:41:26 +1000

    adding more tests

--- src/cmd/test.cpp
+++ src/cmd/test.cpp
@@ -10,6 +10,7 @@ const float PI = 3.14159;
 #define CATCH_CONFIG_MAIN
 #include "..\..\libs\catch.hpp"
 #include "..\shared\shared.h"
+#include "..\shared\gen.h"
 
 TEST_CASE("Init System", "[Shared]") {
 	//we need to initialise the system to seed random vars
@@ -85,24 +86,73 @@ TEST_CASE("Sqr","[Shared]") {
  	REQUIRE(sqr(0.5) == 0.25);
  	REQUIRE(sqr(-0.5) == 0.25);
 }
-
+//@TODO: Test the set coverage and overlap
+//this test requires init system to be run before
 TEST_CASE("Test the shared set functions", "[Controller]") {
-	kNumPop = 1;
-	kForceSetCoverage = true;
+	kNumPop = 100;
+	InitControllers();
+	//during init controllers the function ForceVarBounds is called,
+	//this test checks the validity of the function
+	for(int c = 0; c < kNumPop; c++){
+		for(int i = 0; i < kNumInput; i++) {
+			for(int s = 0; s < cont[c].input[i].num_sets; s++) {
+				//check set is in bounds
+				CHECK(cont[c].input[i].sets[s].centre_x >= cont[c].input[i].low);
+				CHECK(cont[c].input[i].sets[s].centre_x <= cont[c].input[i].high);
 
-	SECTION( "CheckSet") {
+				CHECK((cont[c].input[i].sets[s].centre_x - cont[c].input[i].sets[s].left_base) >= cont[c].input[i].low);
+				CHECK((cont[c].input[i].sets[s].centre_x - cont[c].input[i].sets[s].left_top) >= cont[c].input[i].low);
+			
+				CHECK((cont[c].input[i].sets[s].centre_x + cont[c].input[i].sets[s].right_base) <= cont[c].input[i].high);
+				CHECK((cont[c].input[i].sets[s].centre_x + cont[c].input[i].sets[s].right_top) <= cont[c].input[i].high);
 
-	}
-	SECTION( "ForceBounds") {
+				//check set looks like a set
+				CHECK(cont[c].input[i].sets[s].left_base  >= 0);
+				CHECK(cont[c].input[i].sets[s].left_top   >= 0);
+				CHECK(cont[c].input[i].sets[s].right_base >= 0);
+				CHECK(cont[c].input[i].sets[s].right_top  >= 0);
 
+				CHECK(cont[c].input[i].sets[s].left_base  >= cont[c].input[i].sets[s].left_top);
+				CHECK(cont[c].input[i].sets[s].right_base  >= cont[c].input[i].sets[s].right_top);
+			}
+		}
 	}
 }
-
+//this test requires init controllers to be run before
 TEST_CASE("Test the shared controller functions", "[Controller]") {
-	kNumPop = 1;
-	kForceSetCoverage = true;
-
 	SECTION( "ResetAccumulator") {
+		int controller = GetRandInt(0, kNumPop - 1);
+		float value_collection = 0;
+		float scale_collection = 0;
+		InitSimulation(controller);
+		RunSim(controller);
+
+		//confirm that pre reset the accumulator has data
+		for(int r = 0; r < cont[controller].output[0].num_rules; r++){
+
+			value_collection += cont[controller].output[0].value[r];
+			scale_collection += cont[controller].output[0].scale[r];
+		}
+		CHECK_FALSE(value_collection == 0);
+		CHECK_FALSE(scale_collection == 0);
+		CHECK_FALSE(cont[controller].output[0].num_active == 0);
+		CHECK_FALSE(cont[controller].output[0].output == 0);
+
+
+		ResetAccumulator(controller, 0);
+
+		value_collection = 0;
+		scale_collection = 0;
+		//confirm that the accumulator is empty
+		for(int r = 0; r < cont[controller].output[0].num_rules; r++){
+
+			value_collection += cont[controller].output[0].value[r];
+			scale_collection += cont[controller].output[0].scale[r];
+		}
+		CHECK(value_collection == 0);
+		CHECK(scale_collection == 0);
+		CHECK(cont[controller].output[0].num_active == 0);
+		CHECK(cont[controller].output[0].output == 0);
 
 	}
 	SECTION( "CleanController") {
